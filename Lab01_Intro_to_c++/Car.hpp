e66fb666193fc58128a57ab762d8b168ca4e397e
ashfak234 <21336445@stu.mmu.ac.uk>
2025-01-30 14:10:15 +0000

    implemented classes and objects using a header file

---
+++ Lab01_Intro_to_c++/Car.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+
+class Car {
+
+public:
+
+    std::string make;
+    std::string model;
+    int year;
+    float speed = 0.0f;
+
+    // Constructor
+
+    Car(const std::string, const std::string, const int);
+
+    // Methods
+    void outputDetails();
+    void accelerate(const float);
+    static float mph2kph(const float);
+
+};
--- Lab01_Intro_to_c++/Lab01_Intro_to_c++.cpp
+++ Lab01_Intro_to_c++/Lab01_Intro_to_c++.cpp
@@ -1,65 +1,14 @@
-
 #include <iostream>
-
-class Car {
-public:
-    std::string make;
-    std::string model;
-    int year;
-    float speed = 0.0f;
-    // Methods
-    void outputDetails();
-    void accelerate(const float);
-
-    // Constructor
-    Car();
-    Car(const std::string, const std::string, const int);
-
-    static float mph2kph(const float);
-};
-
-
-Car::Car()
-{
-    std::cout << "\nCar object created" << std::endl;
-}
-
-Car::Car(const std::string makeInput, const std::string modelInput, const int yearInput)
-{
-    make = makeInput;
-    model = modelInput;
-    year = yearInput;
-    std::cout << "\nCar object created" << std::endl;
-}
-
-float Car::mph2kph(const float speed)
-{
-    return speed * 1.60934f;
-}
-
-void Car::outputDetails()
-{
-    std::cout << "\nMake: " << make
-        << "\nModel: " << model
-        << "\nYear: " << year << std::endl;
-}
-
-void Car::accelerate(const float increment)
-{
-    speed = +increment;
-    std::cout << "\nThe car has accelerated to " << speed << " mph." << std::endl;
-}
+#include "Car.hpp"
 
 int main()
 {
-    std::cout << "hello world\n" << std::endl;
-
-
     //// Define car object
     //Car delorean;
     //delorean.make = "Delorean";
     //delorean.model = "DMC-12";
     //delorean.year = 1981;
+
     Car delorean("DeLorean", "DMC-12", 1981);
     std::cout << "\nClasses\n-------" << std::endl; // "Classes: "
     delorean.outputDetails();
